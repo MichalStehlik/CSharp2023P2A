755c067ee9689aeba2da2059b1c1801db6f0461a
Michal Stehlík <michal.stehlik@pslib.cz>
2023-10-23 13:27:01 +0200

    oop1

---
+++ CSharp03OOP/Book.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp03OOP
+{
+    internal class Book
+    {
+        private string _title = default; // datová položka, atribut
+        private string _author;
+        // implicitní konstruktor
+
+        public void SetTitle(string title) // metoda
+        {
+            if (title != "XXXXXX")
+            {
+                _title = title;
+            }
+            // this.title = title;
+        }
+        public string GetAuthor() // metoda
+        {
+            return _author;
+        }
+
+        public void SetAuthor(string author) // metoda
+        {
+            if (author != "")
+            {
+                _author = author;
+            }
+            // this.title = title;
+        }
+        public string GetTitle() // metoda
+        {
+            return _title;
+        }
+
+        public string GetName()
+        {
+            return _author + ": " + _title;
+        }
+
+        public Book(string title) // konstruktor
+        {
+            _title = title;
+            _author = "?";
+        }
+
+        /*
+        public Book(string title, string author) // konstruktor, přetížení konstruktorů
+        {
+            _title = title;
+            _author = author;
+        }
+        */
+        public Book(string title, string author = "?") // konstruktor, implicitní parametr
+        {
+            _title = title;
+            _author = author;
+        }
+
+        ~Book() // destruktor
+        {
+            // úklid objektu
+        }
+
+        // vlastnosti
+        // inicializace objektu
+        // zapouzdření a dědičnost
+    }
+}
---
+++ CSharp03OOP/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more informationvar 
+using CSharp03OOP;
+
+var sp = new Book("Společenstvo prstenů");
+// sp.title = "";
+sp.SetTitle("Společenstvo prstenů");
+Console.WriteLine(sp.GetTitle());
+var tt = new Book("Dvě věže","JRRT");
+
+Book[] books = new Book[2];
+books[0] = sp;
+books[1] = tt;
+foreach (var book in books)
+{
+    Console.WriteLine(book.GetName());
+}
+/*
+string[] names = new string[2];
+string[] authors = new string[2];
+names[0] = "Babička";
+authors[0] = "Němcová";
+*/
