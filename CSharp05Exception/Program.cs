3d7012ff03ba8b8e5205626fd480563315368107
Michal Stehlík <michal.stehlik@pslib.cz>
2023-11-06 13:30:54 +0100

    exception

---
+++ CSharp05Exception/Program.cs
@@ -0,0 +1,38 @@
+﻿using CSharp05Exception;
+
+int a = 10;
+int b = 0;
+int c = 0;
+try
+{
+    // uživatelský vstup - TryParse
+    // souborové operace
+    // databáze, internet
+    // hardware
+    c = a / b; // -- exception
+    Console.WriteLine("Vše OK"); // proběhne jen, pokud není chyba
+}
+catch(DivideByZeroException ex)
+{
+    // kód proběhne při chybě
+    c = int.MaxValue;
+    Console.WriteLine("Dělíš nulou: " + ex.Message);
+}
+catch(Exception ex)
+{
+    Console.WriteLine("Neznámá chyba: " + ex.Message);
+}
+finally
+{
+    // kód proběhne vždy
+    Console.WriteLine("Done.");
+}
+Console.WriteLine(c);
+try
+{
+    Rectangle x = new Rectangle(0);
+}
+catch(ArgumentOutOfRangeException ex)
+{
+    Console.WriteLine("Obdélník nelze sestrojit:" + ex.Message);
+}
---
+++ CSharp05Exception/Rectangle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp05Exception
+{
+    internal class Rectangle
+    {
+        private double _x;
+
+        public Rectangle(double x)
+        {
+            X = x;
+        }
+
+        public double X 
+        { 
+            get 
+            { 
+                return _x;
+            } 
+            set 
+            {
+                if (value <= 0)
+                {
+                    //throw new ArgumentOutOfRangeException("X must be above zero");
+                    throw new RectangleException("X must be above zero");
+                }
+                _x = value;
+            } 
+        }
+    }
+
+    class RectangleException : Exception
+    {
+        public RectangleException()
+        {
+        }
+
+        public RectangleException(string? message) : base(message)
+        {
+        }
+
+        public RectangleException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected RectangleException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+
+    // https://learn.microsoft.com/en-us/dotnet/standard/exceptions/
+}
