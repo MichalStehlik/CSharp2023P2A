3dbf9ad443a40bbfb24a063ea0726796f9d9379b
Michal Stehlík <michal.stehlik@pslib.cz>
2024-01-29 13:27:47 +0100

    delegates

---
+++ CSharp12Delegates/DelegateExample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp12Delegates
+{
+    internal class DelegateExample
+    {
+        public DelegateExample(float a, float b)
+        {
+            A = a;
+            B = b;
+        }
+
+        public float Addition(float a, float b) { return a + b; }
+        public float Subtraction(float a, float b) { return a - b; }
+        public float Multiplication(float a, float b) { return a * b; }
+        public float Division(float a, float b) { return a / b; }
+
+        public float Do(NumericOperation op)
+        {
+            return op(A,B);
+        }
+
+        public float A { get; set; }
+        public float B { get; set; }
+    }
+
+    delegate float NumericOperation(float a, float b);
+}
---
+++ CSharp12Delegates/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CSharp12Delegates;
+
+var del = new DelegateExample(7,3);
+Console.WriteLine(del.Do(del.Addition));
+Console.WriteLine(del.Do(del.Subtraction));
+Console.WriteLine(del.Do(del.Multiplication));
+Console.WriteLine(del.Do(del.Division));
+Console.WriteLine(del.Do(SpecialMath));
+Console.WriteLine(del.Do(delegate (float x, float y) { return x * x / y; }));
+Console.WriteLine(del.Do((x,y) => { return x * x / y; }));
+Console.WriteLine(del.Do((x, y) => (x * x / y))); // lambda op // arrow fce
+Console.WriteLine();
+List<int> ints = new List<int> {41,14,85,6,87,1,2,4,8,61,2,14,7 };
+foreach (int i in ints.Where(x => x > 10)) // LINQ
+{
+    //if (i > 10)
+        Console.WriteLine(i);
+}
+Console.WriteLine();
+foreach (int i in ints.OrderBy(x => x))
+{
+    //if (i > 10)
+    Console.WriteLine(i);
+}
+Console.WriteLine();
+foreach (int i in ints.OrderByDescending(x => x))
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine();
+foreach (int i in ints.Where(x => x % 2 == 0).OrderBy(x => x))
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine();
+foreach (int i in ints
+    .Where(x => x % 2 == 0)
+    .OrderBy(x => x)
+    .Select(x => 10*x)
+    .Where(x => x > 50))
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine();
+float SpecialMath(float x, float y)
+{
+    return x * x * y;
+}
